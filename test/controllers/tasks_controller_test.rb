9248d1078712d4244d2e6ec2d9101bb8131a0b48
Dz0526 <shining.sun0526@gmail.com>
2021-04-22 23:11:34 +0900

    feat(general): made basic mvc

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -4,4 +4,9 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "shoud_get_root" do
+    get root_url
+    assert_response :success
+  end
 end
